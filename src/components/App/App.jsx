3e812cdc3635d179ba42b03bf74dc4caa3ca0646
Nema Problema <37379898+fjende@users.noreply.github.com>
2020-01-16 16:37:09 +0100

    Added Login, Register frontend. -- ESLINT ERROR --

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,8 +1,18 @@
 import React, { Component } from 'react';
+import Login from '../Pages/Login/Login';
+import Register from '../Pages/Login/Register';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
 export class App extends Component {
   render() {
-    return <div>Hello my man!</div>;
+    return (
+      <Router>
+        <Switch>
+          <Route path="/" exact component={Login} />
+          <Route path="/register" component={Register} />
+        </Switch>
+      </Router>
+    );
   }
 }
 
