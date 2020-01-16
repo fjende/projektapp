a64886041e6ffcaa07a077ed2a546a533dd9f5eb
Nema Problema <37379898+fjende@users.noreply.github.com>
2020-01-16 17:03:20 +0100

    Added /Home /Nav folders, Theme propety to App.jsx

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,16 +2,29 @@ import React, { Component } from 'react';
 import Login from '../Pages/Login/Login';
 import Register from '../Pages/Login/Register';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import { createMuiTheme } from '@material-ui/core/styles'
+import { ThemeProvider } from '@material-ui/styles';
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: '#55E6C1',
+      contrastText: "#FFFFFF",
+    },
+  },
+});
 
 export class App extends Component {
   render() {
     return (
-      <Router>
-        <Switch>
-          <Route path="/" exact component={Login} />
-          <Route path="/register" component={Register} />
-        </Switch>
-      </Router>
+      <ThemeProvider theme={theme}>
+        <Router>
+          <Switch>
+            <Route path="/" exact component={Login} />
+            <Route path="/register" component={Register} />
+          </Switch>
+        </Router>
+      </ThemeProvider>
     );
   }
 }
