37d4f16d0905016e5833c36d68d11071be57433b
Nema Problema <37379898+fjende@users.noreply.github.com>
2020-01-16 21:02:01 +0100

    Added Responsive Drawer from Material UI, todo: split Nav & Home

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Login from '../Pages/Login/Login';
 import Register from '../Pages/Login/Register';
+import Home from '../Pages/Home/Home';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import { createMuiTheme } from '@material-ui/core/styles'
 import { ThemeProvider } from '@material-ui/styles';
@@ -8,10 +9,15 @@ import { ThemeProvider } from '@material-ui/styles';
 const theme = createMuiTheme({
   palette: {
     primary: {
-      main: '#55E6C1',
+      main: '#82ccdd',
       contrastText: "#FFFFFF",
     },
+    secondary: {
+      main: '#323133',
+      contrastText: "#FFFFFF",
+    }
   },
+  shadows: ["none"],
 });
 
 export class App extends Component {
@@ -22,6 +28,7 @@ export class App extends Component {
           <Switch>
             <Route path="/" exact component={Login} />
             <Route path="/register" component={Register} />
+            <Route path="/home" exact component={Home} />
           </Switch>
         </Router>
       </ThemeProvider>
