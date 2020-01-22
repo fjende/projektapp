d3fbe7e03798a168f0c5084cd66756e782911f45
Nema Problema <37379898+fjende@users.noreply.github.com>
2020-01-22 16:23:32 +0100

    Split Nav to seperate compontent.

--- src/components/Pages/Profile/ProfileContent.jsx
+++ src/components/Pages/Profile/ProfileContent.jsx
@@ -3,6 +3,7 @@ import { makeStyles } from '@material-ui/core/styles';
 import Select from '@material-ui/core/Select';
 import Grid from '@material-ui/core/Grid';
 import Paper from '@material-ui/core/Paper';
+import { Typography } from '@material-ui/core/';
 
 const useStyles = makeStyles(theme => ({
     root: {
@@ -17,10 +18,49 @@ export default function StatisticsContent() {
     return (
         <div className={classes.root}>
 
-            <Grid container spacing={2} justify="center"
-                alignItems="center">
-
-            </Grid>
+            <Typography variant="h1" component="h2" gutterBottom>
+                h1. Heading
+      </Typography>
+            <Typography variant="h2" gutterBottom>
+                h2. Heading
+      </Typography>
+            <Typography variant="h3" gutterBottom>
+                h3. Heading
+      </Typography>
+            <Typography variant="h4" gutterBottom>
+                h4. Heading
+      </Typography>
+            <Typography variant="h5" gutterBottom>
+                h5. Heading
+      </Typography>
+            <Typography variant="h6" gutterBottom>
+                h6. Heading
+      </Typography>
+            <Typography variant="subtitle1" gutterBottom>
+                subtitle1. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos blanditiis tenetur
+      </Typography>
+            <Typography variant="subtitle2" gutterBottom>
+                subtitle2. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos blanditiis tenetur
+      </Typography>
+            <Typography variant="body1" gutterBottom>
+                body1. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos blanditiis tenetur
+                unde suscipit, quam beatae rerum inventore consectetur, neque doloribus, cupiditate numquam
+                dignissimos laborum fugiat deleniti? Eum quasi quidem quibusdam.
+      </Typography>
+            <Typography variant="body2" gutterBottom>
+                body2. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos blanditiis tenetur
+                unde suscipit, quam beatae rerum inventore consectetur, neque doloribus, cupiditate numquam
+                dignissimos laborum fugiat deleniti? Eum quasi quidem quibusdam.
+      </Typography>
+            <Typography variant="button" display="block" gutterBottom>
+                button text
+      </Typography>
+            <Typography variant="caption" display="block" gutterBottom>
+                caption text
+      </Typography>
+            <Typography variant="overline" display="block" gutterBottom>
+                overline text
+      </Typography>
 
         </div>
     );
--- src/components/Pages/Tasks/Tasks.jsx
+++ src/components/Pages/Tasks/Tasks.jsx
@@ -31,41 +31,22 @@ import InputBase from '@material-ui/core/InputBase';
 import Grid from '@material-ui/core/Grid';
 import TaskList from './TaskList';
 import Paper from '@material-ui/core/Paper';
+import NavBar from '../Nav/NavBar'
 
 const drawerWidth = 290;
 
 const useStyles = makeStyles(theme => ({
     root: {
-        display: 'flex'
-    },
-    drawer: {
-        [theme.breakpoints.up('sm')]: {
-            width: drawerWidth,
-            flexShrink: 0
-        }
-    },
-    appBar: {
-        [theme.breakpoints.up('sm')]: {
-            width: `calc(100% - ${drawerWidth}px)`,
-            marginLeft: drawerWidth
-        }
-    },
-    menuButton: {
-        marginRight: theme.spacing(2),
-        [theme.breakpoints.up('sm')]: {
-            display: 'none'
-        }
-    },
-    datePicker: {
-        marginRight: theme.spacing(1)
-    },
-    toolbar: theme.mixins.toolbar,
-    drawerPaper: {
-        width: drawerWidth
+        display: 'flex',
+        flexGrow: 1,
     },
     content: {
         flexGrow: 1,
-        marginTop: theme.spacing(8)
+        marginTop: theme.spacing(8),
+        [theme.breakpoints.up('sm')]: {
+            width: `calc(100% - ${drawerWidth}px)`,
+            marginLeft: 0
+        }
     }
 }));
 
@@ -73,126 +54,11 @@ function Tasks(props) {
     const { container } = props;
     const classes = useStyles();
     const theme = useTheme();
-    const [mobileOpen, setMobileOpen] = React.useState(false);
-
-    const handleDrawerToggle = () => {
-        setMobileOpen(!mobileOpen);
-    };
-
-    const drawer = (
-        <div>
-            <div className={classes.toolbar} />
-            <Divider />
-            <List style={{ marginLeft: 10 }}>
-                <ListItem button component="a" key="Schedule" href="/home">
-                    <ListItemIcon>
-                        <ScheduleIcon />
-                    </ListItemIcon>
-                    <ListItemText primary="Schedule" />
-                </ListItem>
-
-                <ListItem button component="a" key="Tasks" href="/tasks">
-                    <ListItemIcon>
-                        <AssignmentIcon />
-                    </ListItemIcon>
-                    <ListItemText primary="Tasks" />
-                </ListItem>
-                <ListItem button component="a" key="Categories" href="/categories">
-                    <ListItemIcon>
-                        <CategoryIcon />
-                    </ListItemIcon>
-                    <ListItemText primary="Categories" />
-                </ListItem>
-
-                <ListItem button component="a" key="Statistics" href="/statistics">
-                    <ListItemIcon>
-                        <EqualizerIcon />
-                    </ListItemIcon>
-                    <ListItemText primary="Statistics" />
-                </ListItem>
-                <ListItem button component="a" key='Profile' href="/profile">
-
-                    <ListItemIcon><AccountCircleIcon /></ListItemIcon>
-                    <ListItemText primary='Profile' />
-
-                </ListItem>
-            </List>
-
-            <List style={{ position: 'absolute', bottom: 0, marginLeft: 15 }}>
-                <ListItem
-                    button
-                    component="a"
-                    key="Logout"
-                    onClick={() => {
-                        sessionStorage.removeItem('loggedIn');
-                        sessionStorage.removeItem('userId');
-                        props.history.push('/');
-                    }}
-                >
-                    <ListItemIcon>
-                        <ExitToAppIcon />
-                    </ListItemIcon>
-                    <ListItemText primary="Logout" />
-                </ListItem>
-            </List>
-        </div>
-    );
 
     return (
         <div className={classes.root}>
             <CssBaseline />
-            <AppBar position="fixed" className={classes.appBar} color="secondary">
-                <Toolbar>
-                    <IconButton
-                        color="inherit"
-                        aria-label="open drawer"
-                        edge="start"
-                        onClick={handleDrawerToggle}
-                        className={classes.menuButton}
-                    >
-                        <MenuIcon />
-                    </IconButton>
-                    <Typography variant="h6" className={classes.datePicker}>
-                        Tasks
-          </Typography>
-                    {/**    <div className={classes.datePicker}>
-                        <DatePicker />
-                     </div> **/}
-                </Toolbar>
-            </AppBar>
-            <nav className={classes.drawer} aria-label="drawer-nav">
-                {/* The implementation can be swapped with js to avoid SEO duplication of links. */}
-                <Hidden smUp implementation="css">
-                    <Drawer
-                        container={container}
-                        variant="temporary"
-                        anchor={theme.direction === 'rtl' ? 'right' : 'left'}
-                        open={mobileOpen}
-                        onClose={handleDrawerToggle}
-                        classes={{
-                            paper: classes.drawerPaper
-                        }}
-                        ModalProps={{
-                            keepMounted: true // Better open performance on mobile.
-                        }}
-                    >
-                        {drawer}
-                    </Drawer>
-                </Hidden>
-                <Hidden xsDown implementation="css">
-                    <Drawer
-                        classes={{
-                            paper: classes.drawerPaper
-                        }}
-                        variant="permanent"
-                        open
-                        container={container}
-                    >
-                        {drawer}
-                    </Drawer>
-                </Hidden>
-            </nav>
-
+            <NavBar />
             <div className={classes.content}>
                 <Paper variant="outlined">
                     <TaskList />
@@ -201,13 +67,5 @@ function Tasks(props) {
         </div>
     );
 }
-
-Tasks.propTypes = {
-    /**
-     * Injected by the documentation to work in an iframe.
-     * You won't need it on your project.
-     */
-    container: PropTypes.instanceOf(typeof Element === 'undefined' ? Object : Element)
-};
 
 export default Tasks;
