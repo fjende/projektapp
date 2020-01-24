7d5c46ffbf8d522326a9eab3145be55b7c9984f2
Nema Problema <37379898+fjende@users.noreply.github.com>
2020-01-24 13:00:25 +0100

    minor fix

--- src/components/Modals/EditCategoryModal.jsx
+++ src/components/Modals/EditCategoryModal.jsx
@@ -14,81 +14,81 @@ import axios from 'axios';
 import { API_ENDPOINT } from '../../api';
 
 const styles = {
-  inputs: {
-    marginBottom: '20px',
-    '& > div': {
-      marginBottom: '20px'
+    inputs: {
+        marginBottom: '20px',
+        '& > div': {
+            marginBottom: '20px'
+        }
     }
-  }
 };
 
 export class EditCategoryModal extends Component {
-  constructor(props) {
-    super(props);
+    constructor(props) {
+        super(props);
 
-    this.state = {
-      selectedDate: new Date(),
-      currentaActivityId: 0
+        this.state = {
+            selectedDate: new Date(),
+            currentaActivityId: 0
+        };
+    }
+
+    handleSubmit = (values, formikBag) => {
+        axios
+            .put(`${API_ENDPOINT}/activity-type-daily-schedule/${this.props.currentCategory.id}`, {
+                userId: sessionStorage.getItem('userId'),
+                activityTypeId: this.props.currentCategory.activityTypeId,
+                timeFrom: values.timeFrom.toJSON(),
+                timeTo: values.timeTo.toJSON()
+            })
+            .then(res => this.props.hideModal())
+            .catch(error => alert('Daily Schedule for Activity Type already exists!'));
     };
-  }
 
-  handleSubmit = (values, formikBag) => {
-    axios
-      .put(`${API_ENDPOINT}/activity-type-daily-schedule/${this.props.currentCategory.id}`, {
-        userId: sessionStorage.getItem('userId'),
-        activityTypeId: this.props.currentCategory.activityTypeId,
-        timeFrom: values.timeFrom.toJSON(),
-        timeTo: values.timeTo.toJSON()
-      })
-      .then(res => this.props.hideModal())
-      .catch(error => alert('Daily Schedule for Activity Type already exists!'));
-  };
+    render() {
+        const { classes, hideModal, currentCategory } = this.props;
 
-  render() {
-    const { classes, hideModal, currentCategory } = this.props;
-
-    return (
-      <div className={classes.modal}>
-        <Formik
-          onSubmit={(values, formikBag) => this.handleSubmit(values, formikBag)}
-          initialValues={{ dateFrom: currentCategory.timeFrom, dateTo: currentCategory.timeTo }}
-          render={formikProps => {
-            return (
-              <MuiPickersUtilsProvider utils={DateFnsUtils}>
-                <Form className={classes.form}>
-                  <div className={classes.inputs}>
-                    <KeyboardTimePicker
-                      ampm={false}
-                      inputVariant="outlined"
-                      required
-                      label="Start time"
-                      value={formikProps.values.timeFrom}
-                      minutesStep={5}
-                      onChange={dateFrom => {
-                        formikProps.setFieldValue('timeFrom', dateFrom);
-                      }}
-                    />
-                    <KeyboardTimePicker
-                      ampm={false}
-                      required
-                      inputVariant="outlined"
-                      label="End time"
-                      minutesStep={5}
-                      value={formikProps.values.timeTo}
-                      onChange={dateTo => formikProps.setFieldValue('timeTo', dateTo)}
-                    />
-                    <Button type="submit" fullWidth variant="contained" color="primary">
-                      Reschedule
+        return (
+            <div className={classes.modal}>
+                <Formik
+                    onSubmit={(values, formikBag) => this.handleSubmit(values, formikBag)}
+                    initialValues={{ timeFrom: currentCategory.timeFrom, timeTo: currentCategory.timeTo }}
+                    render={formikProps => {
+                        return (
+                            <MuiPickersUtilsProvider utils={DateFnsUtils}>
+                                <Form className={classes.form}>
+                                    <div className={classes.inputs}>
+                                        <KeyboardTimePicker
+                                            ampm={false}
+                                            inputVariant="outlined"
+                                            required
+                                            label="Start time"
+                                            value={formikProps.values.timeFrom}
+                                            minutesStep={5}
+                                            onChange={timeFrom => {
+                                                formikProps.setFieldValue('timeFrom', timeFrom);
+                                            }}
+                                        />
+                                        <KeyboardTimePicker
+                                            ampm={false}
+                                            required
+                                            inputVariant="outlined"
+                                            label="End time"
+                                            minutesStep={5}
+                                            value={formikProps.values.timeTo}
+                                            onChange={timeTo => formikProps.setFieldValue('timeTo', timeTo)}
+                                        />
+                                        <Button type="submit" fullWidth variant="contained" color="primary">
+                                            Reschedule
                     </Button>
-                  </div>
-                </Form>
-              </MuiPickersUtilsProvider>
-            );
-          }}
-        ></Formik>
-      </div>
-    );
-  }
+                                    </div>
+                                </Form>
+                            </MuiPickersUtilsProvider>
+                        );
+                    }}
+                ></Formik>
+            </div>
+        );
+    }
 }
 
 export default injectSheet(styles)(EditCategoryModal);
