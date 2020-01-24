b8f6b051f0891f71a2ce5dbdc04ce879c6a2b4ab
Nema Problema <37379898+fjende@users.noreply.github.com>
2020-01-24 02:32:03 +0100

    add category modal, need to sync api requests

--- src/components/Modals/AddCategoryModal.jsx
+++ src/components/Modals/AddCategoryModal.jsx
@@ -14,96 +14,102 @@ import axios from 'axios';
 import { API_ENDPOINT } from '../../api';
 
 const styles = {
-  inputs: {
-    marginBottom: '20px',
-    '& > div': {
-      marginBottom: '20px'
+    inputs: {
+        marginBottom: '20px',
+        '& > div': {
+            marginBottom: '20px'
+        }
     }
-  }
 };
 
 export class AddCategoryModal extends Component {
-  constructor(props) {
-    super(props);
+    constructor(props) {
+        super(props);
 
-    this.state = {
-      selectedDate: new Date()
-    };
-  }
+        this.state = {
+            selectedDate: new Date(),
+            currentaActivityId: 0
+        };
+    }
 
-  handleSubmit = async (values, formikBag) => {};
+    postActivityTypeDailySchedule = async (values, formikBag) => {
+        await axios
+            .post(`${API_ENDPOINT}/activity-type-daily-schedule`, {
+                userId: sessionStorage.getItem('userId'),
+                activityTypeId: this.state.currentaActivityId,
+                timeFrom: values.timeFrom.toJSON(),
+                timeTo: values.timeTo.toJSON()
+            })
+            .then(res => console.log(res))
+            .catch(error => alert('Daily Schedule for Activity Type already exists!'));
+    }
 
-  postDailySchedule = (values, formikBag) => {
-    axios
-      .post(`${API_ENDPOINT}/activity-type-daily-schedule`, {
-        name: values.name,
-        userId: sessionStorage.getItem('userId')
-      })
-      .catch(error => alert('Category name already exists!'));
-  };
+    postActivityType = async (values, formikBag) => {
+        await axios
+            .post(`${API_ENDPOINT}/activity-type`, {
+                name: values.name,
+            })
+            .then(res => this.setState({ currentaActivityId: res.data.id }))
+            .then(console.log(this.state.currentaActivityId))
+            .catch(error => alert('Category name already exists!'));
+    }
 
-  postActivityType = async values => {
-    await axios
-      .post(`${API_ENDPOINT}/activity-type`, {
-        name: values.name
-      })
-      .catch(error => alert('Category name already exists!'));
-  };
+    handleSubmit = (values, formikBag) => {
+        Promise.all([this.postActivityType(values, formikBag), this.postActivityTypeDailySchedule(values, formikBag)]);
+    }
 
-  render() {
-    const { classes, hideModal } = this.props;
+    render() {
+        const { classes, hideModal } = this.props;
 
-    return (
-      <div className={classes.modal}>
-        <Formik
-          onSubmit={(values, formikBag) => this.handleSubmit(values, formikBag)}
-          initialValues={{ name: '', date: new Date(), color: '', type: '', status: '' }}
-          render={formikProps => {
-            return (
-              <MuiPickersUtilsProvider utils={DateFnsUtils}>
-                <Form className={classes.form}>
-                  <div className={classes.inputs}>
-                    <TextField
-                      variant="outlined"
-                      margin="normal"
-                      required
-                      fullWidth
-                      id="name"
-                      label="Name"
-                      name="name"
-                      autoFocus
-                    />
-                    <KeyboardTimePicker
-                      ampm={false}
-                      inputVariant="outlined"
-                      label="Start time"
-                      value={formikProps.values.timeFrom}
-                      minutesStep={5}
-                      onChange={date => {
-                        formikProps.setFieldValue('timeFrom', date);
-                        console.log(date);
-                      }}
-                    />
-                    <KeyboardTimePicker
-                      ampm={false}
-                      inputVariant="outlined"
-                      label="End time"
-                      minutesStep={5}
-                      value={formikProps.values.timeTo}
-                      onChange={date => formikProps.setFieldValue('timeTo', date)}
-                    />
-                    <Button type="submit" fullWidth variant="contained" color="primary">
-                      Create
-                    </Button>
-                  </div>
-                </Form>
-              </MuiPickersUtilsProvider>
-            );
-          }}
-        ></Formik>
-      </div>
-    );
-  }
+        return (
+            <div className={classes.modal}>
+                <Formik
+                    onSubmit={(values, formikBag) => this.handleSubmit(values, formikBag)}
+                    initialValues={{ name: '', date: new Date(), color: '', type: '', status: '' }}
+                    render={formikProps => {
+                        return (
+                            <MuiPickersUtilsProvider utils={DateFnsUtils}>
+                                <Form className={classes.form}>
+                                    <div className={classes.inputs}>
+                                        <TextField
+                                            variant="outlined"
+                                            margin="normal"
+                                            required
+                                            fullWidth
+                                            id="name"
+                                            label="Name"
+                                            name="name"
+                                        />
+                                        <KeyboardTimePicker
+                                            ampm={false}
+                                            inputVariant="outlined"
+                                            required
+                                            label="Start time"
+                                            value={formikProps.values.timeFrom}
+                                            minutesStep={5}
+                                            onChange={date => { formikProps.setFieldValue('timeFrom', date); console.log(date) }}
+                                        />
+                                        <KeyboardTimePicker
+                                            ampm={false}
+                                            required
+                                            inputVariant="outlined"
+                                            label="End time"
+                                            minutesStep={5}
+                                            value={formikProps.values.timeTo}
+                                            onChange={date => formikProps.setFieldValue('timeTo', date)}
+                                        />
+                                        <Button type="submit" fullWidth variant="contained" color="primary">
+                                            Create
+                                      </Button>
+                                    </div>
+                                </Form>
+                            </MuiPickersUtilsProvider>
+                        );
+                    }}
+                ></Formik>
+            </div>
+        );
+    }
 }
 
 export default injectSheet(styles)(AddCategoryModal);
